c405b8960841d98d5823de56236cd6b590d094e6
na-siddiky <siddiky.academic@gmail.com>
2022-01-16 17:38:54 +0600

    Exmaple 3.2

---
+++ Chapter 3/P3_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    n = 98899;
+    if (n >= 0)
+    {
+        printf("the number is positive \n");
+    }
+    else
+    {
+        printf("the number is negative \n");
+    }
+    return 0;
+}
