61caa41cad9a43880415c93300c2792d3349d7d7
na-siddiky <siddiky.academic@gmail.com>
2022-01-16 14:18:05 +0600

    Example 2.13

--- Chapter 2/P2_13.c
+++ Chapter 2/P2_13.c
@@ -6,4 +6,10 @@ int main()
     ch = getchar();
     printf("The first letter of your name is: %c\n", ch);
     return 0;
+
+    // char c = "N";
+    // printf("Enter the name of the first letter: ");
+    // c = getchar();
+    // printf("The first letter of your name is: %c\n", c);
+    // return 0;
 }
