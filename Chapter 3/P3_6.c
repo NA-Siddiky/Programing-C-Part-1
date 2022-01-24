ad7e867cf98ca3fd144bff3d5f36cca5d5415c5e
na-siddiky <siddiky.academic@gmail.com>
2022-01-25 01:37:50 +0600

    Exmaple 3.6

---
+++ Chapter 3/P3_6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 10;
+    if (n < 30)
+    {
+        printf("n is less 30.\n");
+    }
+    else if (n < 50)
+    {
+        printf("n is less 50.\n");
+    }
+    return 0;
+}
