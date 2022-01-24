51a50b565dad3ed39539d96dfe6ee905364a3d54
na-siddiky <siddiky.academic@gmail.com>
2022-01-25 02:09:08 +0600

    Exmaple 3.9

---
+++ Chapter 3/P3_9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int number = 101;
+    int x = number / 2;
+    int y = x * 2;
+    if (number / 2 && number == y)
+    {
+        printf("the number is even\n");
+    }
+    else
+    {
+        printf("the number is odd\n");
+    }
+}
