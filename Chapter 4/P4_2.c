19269421d76664291a5b95ef7ce4ed9021510a6d
na-siddiky <siddiky.academic@gmail.com>
2022-01-27 02:37:03 +0600

    Exmaple 4.3

---
+++ Chapter 4/P4_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    // int n;
+    // printf("Enter a number: \n");
+    // scanf("%d", &n);
+
+    int n = 1;
+
+    while (n <= 10)
+    {
+        printf("%d\n", n);
+        n++;
+    }
+    return 0;
+}
