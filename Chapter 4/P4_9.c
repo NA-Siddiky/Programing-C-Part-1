517408fd9a56dd436da33f6b4386d82c0ecbc102
na-siddiky <siddiky.academic@gmail.com>
2022-01-27 16:46:35 +0600

    Exmaple 4.9

---
+++ Chapter 4/P4_9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    int m, n = 5;
+    int i;
+
+    m = 0;
+
+    for (i = 1; i <= 10; i++)
+    {
+        m = m + n;
+        printf("%d X %d = %d\n", n, i, m);
+    }
+}
