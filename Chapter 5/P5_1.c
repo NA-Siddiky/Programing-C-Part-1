47034fc11d03bd5c317bc1dfc45d38a3feb7bb2f
NA-Siddiky <siddiky.academic@gmail.com>
2022-02-05 01:02:15 +0600

    Example 5.2

--- Chapter 5/P5_1.c
+++ Chapter 5/P5_1.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    int n = 1;
-    while (n <= 5)
-    {
-        printf("%d\n", n);
-        n++;
-    }
+    double x, y, x_plus_y, x_minus_y;
+
+    printf("Enter the value of x+y= ");
+    scanf("%lf", &x_plus_y);
+
+    printf("Enter the value of x-y= ");
+    scanf("%lf", &x_minus_y);
+
+    x = (x_plus_y + x_minus_y) / 2;
+    y = (x_plus_y - x_minus_y) / 2;
+
+    printf("x = %0.2lf, y=%0.2lf\n", x, y);
+
     return 0;
 }
