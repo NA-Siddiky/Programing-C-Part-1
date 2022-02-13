1fe40d293352f821d0e0f82b7617fb6d9455fe25
NA-Siddiky <siddiky.academic@gmail.com>
2022-02-13 16:26:05 +0600

    Example 6.4

---
+++ Chapter 6/P6_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    int my_ara[10] = {5, 10, 15, 20, 25, 30, 35, 40, 50, 55};
+
+    printf("%d\n", my_ara[0]);
+    printf("%d\n", my_ara[9]);
+    return 0;
+}
