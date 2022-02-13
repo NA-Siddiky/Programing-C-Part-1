65ef8f9039eab4a944b66cceeaf30e82a9d0749c
NA-Siddiky <siddiky.academic@gmail.com>
2022-02-13 16:29:29 +0600

    Example 6.5

---
+++ Chapter 6/P6_5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int ara[4] = {5, 10, 15, 20};
+
+    ara[0] = 100;
+    ara[2] = 200;
+    ara[3] = 300;
+
+    printf("%d, %d, %d,%d\n", ara[0], ara[1], ara[2], ara[3]);
+}
