dc9a2f30cbb6b03b3d4911cae8ca8fa12e76abb7
NA-Siddiky <siddiky.academic@gmail.com>
2022-02-14 00:58:49 +0600

    Example 6.10 done

--- Chapter 6/P6_10.c
+++ Chapter 6/P6_10.c
@@ -3,11 +3,11 @@ int main(void)
 {
     int ara[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     int i, j, temp;
-    for (i = 0, j = 9; i < 10; i++, j--)
+    for (i = 0, j = 9; i < 5; i++, j--)
     {
-        temp = ara[j];
+        temp = ara[i];
         ara[i] = ara[j];
-        ara[i] = temp;
+        ara[j] = temp;
     }
     for (i = 0; i < 10; i++)
     {
