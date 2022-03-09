e6f89cd37c0c2c784445ac684d669f04935bbd48
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-09 18:52:02 +0600

    p 6_11 done

--- Chapter 6/P6_11.c
+++ Chapter 6/P6_11.c
@@ -6,5 +6,14 @@ int main(void)
         final_masks[40] = {83, 86, 97, 95, 93, 95, 86, 52, 49, 47, 90, 59, 63, 86, 40, 49, 92, 56, 53, 48, 47, 49, 42, 90, 43, 42, 90, 43, 83, 47, 45, 56, 55, 42, 42, 43, 59, 67, 53, 62};
     // printf("length : %d\n", sizeof(final_masks));
     int i;
-    double total_marks[40]
+    double total_marks[40];
+    for (i = 0; i < 40; i++)
+    {
+        total_marks[i] = ft_marks[i] / 4.0 + st_marks[i] / 4.0 + final_masks[i] / 2.0;
+    }
+    for (i = 1; i <= 40; i++)
+    {
+        printf("Roll No: %d\total_marks: %0.0lf\n", i, total_marks[i - 1]);
+    }
+    return 0;
 }
