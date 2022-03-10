1a9eda01b0cef8478a6a2bd1db221a26240a85db
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-10 23:55:29 +0600

    p 6_14 solved

--- Chapter 6/P6_14.c
+++ Chapter 6/P6_14.c
@@ -3,21 +3,19 @@ int main()
 {
     int i, j;
     int total_marks[] = {6, 7, 8, 9, 2, 3, 4, 7, 8, 4, 1};
-    int marks_count[11];
+    int marks_count[10];
 
-    for (i = 0; i < 11; i++)
+    for (i = 0; i < 10; i++)
     {
         marks_count[i] = 0;
     }
-    for (i = 0; i < 12; i++)
+    for (i = 0; i < 11; i++)
     {
         marks_count[total_marks[i]]++;
-
-        for (j = 0; j <= 10; j++)
-        {
-            printf("%d ", marks_count[j]);
-        }
-        printf("\n");
+    }
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d %d   ", i, marks_count[i]);
     }
     return 0;
 }
