1bf6e9b7dd09d85338250f73739b2d9dd35d10c5
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-13 01:07:57 +0600

    c6 p14-done

--- Chapter 6/P6_14.c
+++ Chapter 6/P6_14.c
@@ -2,20 +2,21 @@
 int main()
 {
     int i, j;
-    int total_marks[] = {6, 7, 8, 9, 2, 3, 4, 7, 8, 4, 1};
-    int marks_count[10];
+    int total_marks[] = {6, 7, 4, 6, 9, 7, 6, 2, 4, 3, 4, 1};
+    int marks_count[11];
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 11; i++)
     {
         marks_count[i] = 0;
     }
-    for (i = 0; i < 11; i++)
+    for (i = 0; i < 12; i++)
     {
         marks_count[total_marks[i]]++;
-    }
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d %d   ", i, marks_count[i]);
+        for (j = 0; j <= 10; j++)
+        {
+            printf("%d ", marks_count[j]);
+        }
+        printf("\n");
     }
     return 0;
 }
