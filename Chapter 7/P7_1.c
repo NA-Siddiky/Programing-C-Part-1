1147936b18aa591eb735511a5aa09022ba159aa3
NA-Siddiky <siddiky.academic@gmail.com>
2022-03-12 23:55:29 +0600

    c7 p1-done

--- Chapter 7/P7_1.c
+++ Chapter 7/P7_1.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int i, j;
-    int total_marks[] = {6, 7, 8, 9, 2, 3, 4, 7, 8, 4, 1};
-    int marks_count[10];
-
-    for (i = 0; i < 10; i++)
-    {
-        marks_count[i] = 0;
-    }
-    for (i = 0; i < 11; i++)
-    {
-        marks_count[total_marks[i]]++;
-    }
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d %d   ", i, marks_count[i]);
-    }
+    double a, b, c;
+    a = 2.5;
+    b = 2.5;
+    c = a + b;
+    printf("%lf\n", c);
     return 0;
 }
