c537b97000cf3ff6231e581813c5db357ddf7630
NA-Siddiky <siddiky@Siddiky-M1.local>
2022-04-30 15:03:08 +0600

    push from mac

---
+++ Chapter 10/test10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i1, i2, add;
+    printf("input number1: \n");
+    scanf("%d", &i1);
+    printf("input number2: \n");
+    scanf("%d", &i2);
+
+    add = i1 + i2;
+    printf("output number is: %d\n", add);
+
+    return 0;
+}
+`
